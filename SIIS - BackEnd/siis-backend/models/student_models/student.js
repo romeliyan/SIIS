db2a1b9b667034fc71d157b412b5b6f7d492914e
melan96 <it17006712@my.sliit.lk>
2019-06-21 21:13:29 +0530

    student-schema initializied

--- SIIS - BackEnd/siis-backend/models/student_models/student.js
+++ SIIS - BackEnd/siis-backend/models/student_models/student.js
@@ -1,60 +1,72 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const Schema = mongoose.Schema;
 
 const studentSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 20
+  },
 
-    firstName: {
-        type: String,
-        required: true,
-        minlength: 2,
-        maxlength: 20
-    },
+  lastName: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30
+  },
 
-    lastName: {
-        type: String,
-        required: true,
-        minlength: 2,
-        maxlength: 30
-    },
+  email: {
+    type: String,
+    required: true,
+    minlength: 6,
+    maxlength: 50,
+    unique: true
+  },
 
-    email: {
-        type: String,
-        required: true,
-        minlength: 6,
-        maxlength: 50,
-        unique: true
-    },
+  nic: {
+    type: String,
+    required: true,
+    minlength: 10,
+    maxlength: 15,
+    unique: true
+  },
 
-    nic: {
-        type: String,
-        required: true,
-        minlength: 10,
-        maxlength: 15,
-        unique: true
-    },
+  mobileNumber: {
+    type: String,
+    required: true,
+    minlength: 10,
+    maxlength: 10,
+    unique: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
 
-    mobileNumber: {
-        type: String,
-        required: true,
-        minlength: 10,
-        maxlength: 10,
-        unique: true
-    },
+  course: {
+    type: [String],
+    required: true
+  },
 
-    studentId: {
-        type: String,
-        required: true,
-        minlength: 10,
-        maxlength: 10,
-        unique: true
-    },
+  studentId: {
+    type: String,
+    required: true,
+    minlength: 10,
+    maxlength: 10,
+    unique: true
+  },
 
-    password: {
-        type: String,
-        minlength: 5,
-        maxlength: 30
-    }
-
+  password: {
+    type: String,
+    minlength: 5,
+    maxlength: 30
+  }
 });
 
-module.exports = mongoose.model('student', StudentSchema);
+const Student = mongoose.model("Student", studentSchema);
+
+module.exports = Student;

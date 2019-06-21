c91841e897c72ae458008e230aebb5b7eb8236f5
romeliyan <rkbliyanage@gmail.com>
2019-06-21 16:55:26 +0530

    models

--- SIIS - BackEnd/siis-backend/models/instructor_models/instructor.js
+++ SIIS - BackEnd/siis-backend/models/instructor_models/instructor.js
@@ -41,14 +41,6 @@ const instructorSchema = new Schema({
         unique: true
     },
 
-    instructorId: {
-        type: String,
-        required: true,
-        minlength: 10,
-        maxlength: 10,
-        unique: true
-    },
-
     password: {
         type: String,
         minlength: 5,
@@ -56,5 +48,22 @@ const instructorSchema = new Schema({
     }
 
 });
+
+const Instructor = mongoose.model('Instructor', instructorSchema);
+
+function validateAdmin(instructor) {
+    const instructorValidateSchema = {
+        firstName: Joi.string().required().minlength(2).maxlength(20),
+        lastName: Joi.string().required().minlength(2).maxlength(30),
+        email: Joi.string().required().minlength(6).maxlength(50).unique(),
+        nic: Joi.string().required().minlength(10).maxlength(15).unique(),
+        mobileNumber: Joi.string().required().minlength(10).maxlength(10).unique(),
+        password: Joi.string().minlength(5).maxlength(30)
+    };
+
+    return Joi.validate(instructor, instructorValidateSchema);
+}
+
+module.exports = mongoose.model('instructor', instructorSchema);
 
 module.exports = mongoose.model('instructor', instructorSchema);

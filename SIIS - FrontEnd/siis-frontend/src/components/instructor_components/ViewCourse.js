4c5c6d0fc50e50a8d726627699c36a462ce91a78
Pasindu <pasindu0996@gmail.com>
2019-06-23 04:41:22 +0530

    Exam, Assignment Routes Updated To Provide Based On CourseName Query Parameter

--- SIIS - FrontEnd/siis-frontend/src/components/instructor_components/ViewCourse.js
+++ SIIS - FrontEnd/siis-frontend/src/components/instructor_components/ViewCourse.js
@@ -5,8 +5,47 @@ class ViewCourse extends Component{
 
   render(){
     return (
-      <div>
-          Hello, World. This is view course
+      <div className="container-fluid">
+        <br/> <br/>
+        <section className="main">
+          <section className="tab-content">
+            <section className="tab-pane active fade in content" id="dashboard">
+              <div className="col-xs-12 col-sm-6">
+                <div className="panel panel-default">
+                  <div className="panel-heading">
+                    <h5>Currently Available Examinations</h5>
+                  </div>
+
+                  <div className="panel-body">
+                    We are a leading non-state degree awarding institute
+                    approved by the University Grants Commission (UGC) under the
+ 
+                  </div>
+
+                </div>
+              </div>
+              
+              <div className="col-xs-12 col-sm-6">
+                <div className="panel panel-default">
+                  <div className="panel-heading">
+                    <h5>Currently Available Assignments</h5>
+                  </div>
+
+                  <div className="panel-body">
+                    We are a leading non-state degree awarding institute
+                    approved by the University Grants Commission (UGC) under the
+ 
+                  </div>
+
+                </div>
+              </div>
+              
+            </section>
+           
+            <section className="tab-pane fade" id="users" />
+            <section className="tab-pane fade" id="mail" />
+          </section>
+        </section>
       </div>
       
     );

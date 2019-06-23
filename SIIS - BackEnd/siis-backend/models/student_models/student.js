beb4528ba599dcdafa7f3d259ab3b6cee30fd2c1
melan96 <it17006712@my.sliit.lk>
2019-06-23 06:48:39 +0530

    joi validation for schemas

--- SIIS - BackEnd/siis-backend/models/student_models/student.js
+++ SIIS - BackEnd/siis-backend/models/student_models/student.js
@@ -20,4 +20,25 @@ const studentSchema = new Schema({
   }
 });
 
+//Joi validation
+function validateStudent(student) {
+  const studentValidateSchema = {
+    firstName: Joi.string().required(),
+    lastName: Joi.string().required(),
+    email: Joi.string().email({ minDomainAtoms: 2 }),
+    nic: Joi.string().required(),
+    mobileNumber: Joi.string().required(),
+    username: Joi.string()
+      .alphanum()
+      .min(3)
+      .max(30)
+      .required(),
+
+    password: Joi.string().required()
+  };
+
+  return Joi.validate(student, studentValidateSchema);
+}
+
+module.exports.ValidateExam = validateStudent;
 module.exports = mongoose.model("students", studentSchema);

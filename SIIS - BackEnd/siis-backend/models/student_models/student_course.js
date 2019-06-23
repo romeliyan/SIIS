576d30aa5017d5d5c321f88f5503ae264af5a88c
melan96 <it17006712@my.sliit.lk>
2019-06-23 06:31:40 +0530

    assignment fetch from remote db with private defined schema

--- SIIS - BackEnd/siis-backend/models/student_models/student_course.js
+++ SIIS - BackEnd/siis-backend/models/student_models/student_course.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 
-const student_course = new mongoose.Schema({
+const student_enrolled_at = new mongoose.Schema({
   studentID: String,
-  courseID: String
+  courseID: String,
+  name: String,
+  lecture: String,
+  code: String
 });
 
-module.exports = mongoose.model("student_course", student_course);
+module.exports = mongoose.model("student_enrolled_at", student_enrolled_at);
--- SIIS - BackEnd/siis-backend/routes/student_routes/student.server.routes.js
+++ SIIS - BackEnd/siis-backend/routes/student_routes/student.server.routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 
 const Student = require("../../models/student_models/student");
-const student_course = require("../../models/student_models/student_course");
+const student_enrolled = require("../../models/student_models/student_course");
+const assignment = require("../../models/student_models/student_assignment");
 const router = express.Router();
 
 router.post("/", (req, res) => {
@@ -41,6 +42,78 @@ router.get("/email/:email", (req, res) => {
 router.get("/user/:id", (req, res) => {
   console.log(req.params.id);
   Student.findById(req.params.id, function(err, doc) {
+    if (err) {
+      console.log(err);
+      res.json({ message: "not found" }).status(404);
+    }
+    console.log(doc);
+    res.send(doc).status(200);
+  });
+});
+
+//POST student when StudentID Recived
+router.post("/enrolled", (req, res) => {
+  let enrolledCourse = new student_enrolled(req.body);
+
+  enrolledCourse
+    .save()
+    .then(response => {
+      console.log(response);
+      res.send(response).status(200);
+    })
+    .catch(err => console.log(err));
+});
+
+//getModulesWhen passing studentID
+
+router.get("/enrolled/:studentID", (req, res) => {
+  console.log(req.params.studentID);
+  student_enrolled.find({ studentID: req.params.studentID }, function(
+    err,
+    doc
+  ) {
+    if (err) {
+      console.log(err);
+      res.json({ message: "not found" }).status(404);
+    }
+    console.log(doc);
+    res.send(doc).status(200);
+  });
+});
+
+//assignment Routes for student purpose
+
+//POST --
+
+router.post("/assignments", (req, res) => {
+  let assignment_inst = new assignment(req.body);
+
+  assignment_inst
+    .save()
+    .then(response => {
+      console.log(response);
+      res.send(response).status(200);
+    })
+    .catch(err => console.log(err));
+});
+
+//GET
+router.get("/assignments/:courseID", (req, res) => {
+  console.log(req.params.courseID);
+  assignment.find({ courseID: req.params.courseID }, function(err, doc) {
+    if (err) {
+      console.log(err);
+      res.json({ message: "not found" }).status(404);
+    }
+    console.log(doc);
+    res.send(doc).status(200);
+  });
+});
+
+//GET ALL --
+router.get("/assignments", (req, res) => {
+  
+  assignment.find({}, function(err, doc) {
     if (err) {
       console.log(err);
       res.json({ message: "not found" }).status(404);
